a9638e64cf85572a7727a6a903ad241c74db9c6f
28G08 <galuh.gsnh@gmail.com>
2021-11-10 11:58:24 +0700

    update

---
+++ LATIHAN/02/latihan.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Makanan</title>
+</head>
+
+<body>
+  <h3>Daftar Makanan</h3>
+
+  <table border="1" cellpad="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>Id Makanan</th>
+      <th>Nama Makanan</th>
+      <th>Rasa</th>
+      <th>Harga</th>
+      <th>Opsi</th>
+    </tr>
+
+    <tr>
+      <td>1</td>
+      <td><img src="bakso.jpg"></td>
+      <td>1</td>
+      <td>Bakso Beranak</td>
+      <td>Gurih</td>
+      <td>20000</td>
+      <td>
+        <a href="">ubah</a> | <a href="">hapus</a>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
